ea94521c94a2e769bc878eb08fe78369ec9e9012
asqubit <49931291+asqubit@users.noreply.github.com>
2019-08-07 12:44:26 +0800

    array fix
    to be compatible with the latest numpy and scipy

--- examples/notch_port_example.py
+++ examples/notch_port_example.py
@@ -3,7 +3,7 @@ from resonator_tools import circuit
 
 
 port1 = circuit.notch_port()
-port1.add_froms2p('S21testdata.s2p',3,4,'realimag',fdata_unit=1e9,delimiter=None)
+port1.add_froms2p(r'examples\S21testdata.s2p',3,4,'realimag',fdata_unit=1e9,delimiter=None)
 port1.autofit()
 print("Fit results:", port1.fitresults)
 port1.plotall()
